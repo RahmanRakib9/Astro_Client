98a8313636c926dd15787703f7ba4a3c80293754
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2023-10-29 21:33:06 +0600

    update bottom header section and install react icon

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import logo from "../../../assets/logo.png"
+import moment from 'moment';
+import { Container, Button, Nav, Navbar, InputGroup, Form } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>
-            {/* Replace this image and content with my own */}
-            <img src={logo} alt="Application logo" />
-            <h4>Journalism Without Fear or Favour</h4>
-            
-        </div>
+        <Container className='mt-5'>
+            {/* Top header section */}
+            <section>
+                <div className='text-center'>
+                    <img src={logo} alt="" />
+                    <p><small>Astronomy compels the soul to look upwards and leads us from one world to another.</small></p>
+                    <p >{moment().format('MMMM Do YYYY')}</p>
+                </div>
+
+            </section>
+
+
+            {/* Bottom header section */}
+            <section>
+                <InputGroup className="mb-3">
+                    <InputGroup.Text></InputGroup.Text>
+                    <Form.Control aria-label="Amount (to the nearest dollar)" />
+                    {/* <InputGroup.Text>.00</InputGroup.Text> */}
+                </InputGroup>
+            </section>
+        </Container>
     );
 };
 
