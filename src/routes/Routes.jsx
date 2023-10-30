2e003f45c6ecfb376e89c49115cbe99d2ebdec4d
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2023-10-30 17:06:56 +0600

    create a user registration form

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,11 +3,16 @@ import {
     createBrowserRouter,
 } from "react-router-dom";
 import RootLayout from "../layout/RootLayout"
+import SignUp from '../userAuthentication/SignUp/SignUp';
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <RootLayout />
+        element: <RootLayout />,
+    },
+    {
+        path: "/register",
+        element: <SignUp></SignUp>
     }
 ])
 
---
+++ src/userAuthentication/SignUp/SignUp.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+
+const SignUp = () => {
+    return (
+        <Container>
+            <section>
+                <div>
+                    <form action=""></form>
+                    {/* name Field */}
+                    <Form.Group className="mb-3" controlId="name">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="email" placeholder="Enter Your Name here" className='w-25' />
+                    </Form.Group>
+                    {/* Email Field */}
+                    <Form.Group className="mb-3" controlId="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Enter Your Email here" className='w-25' />
+                    </Form.Group>
+                    {/* Password Field */}
+                    <Form.Group className="mb-3" controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Enter Your Password here" className='w-25' />
+                    </Form.Group>
+                    {/* Submit Button Field */}
+                    <Form.Group className="mb-3" controlId="submitBtn">
+                        <Button variant='dark' size='lg' active className='w-25'>Submit</Button>
+                    </Form.Group>
+
+                </div>
+            </section>
+        </Container>
+    );
+};
+
+export default SignUp;
