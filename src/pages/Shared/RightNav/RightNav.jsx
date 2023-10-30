88b275dfbc48a6881313661b2982137f9bbe0861
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2023-10-30 13:42:18 +0600

    add social media link

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, ListGroup } from 'react-bootstrap';
 import { FaGoogle, FaGithub, FaFacebook, FaInstagram, FaTwitter, FaGithubAlt, FaLinkedin } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import "./RightNav.css"
 
 const RightNav = () => {
     return (
@@ -20,11 +21,11 @@ const RightNav = () => {
             {/* Social Media option */}
             <div className='mt-3'>
                 <ListGroup>
-                    <ListGroup.Item><FaFacebook /> Facebook</ListGroup.Item>
-                    <ListGroup.Item><FaLinkedin /> Linkedin </ListGroup.Item>
-                    <ListGroup.Item><FaGithubAlt /> Github</ListGroup.Item>
-                    <ListGroup.Item><FaInstagram /> Instagram</ListGroup.Item>
-                    <ListGroup.Item><FaTwitter /> Twitter</ListGroup.Item>
+                    <a href="https://www.facebook.com/RahmanRakibb"><ListGroup.Item ><FaFacebook /> Facebook </ListGroup.Item></a>
+                    <a href="https://www.linkedin.com/in/rahmanrakib9/"><ListGroup.Item ><FaLinkedin /> Linkedin </ListGroup.Item></a>
+                    <a href="https://github.com/RahmanRakib9"><ListGroup.Item ><FaGithub /> Github</ListGroup.Item></a>
+                    <a href="https://www.instagram.com/rahman_rakib9/"><ListGroup.Item ><FaInstagram /> Instagram</ListGroup.Item></a>
+                    <a href="https://twitter.com/RahmanRakib10"><ListGroup.Item ><FaTwitter /> Twitter</ListGroup.Item></a>
 
                 </ListGroup>
             </div>
