2ec109cb6aca6dd7edb4cd9aeb0a8488cf89b1f7
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2023-10-30 13:10:13 +0600

    fixed application layout

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import Header from '../pages/Shared/Header/Header';
 import Footer from '../pages/Shared/Footer/Footer';
+import { Outlet } from 'react-router-dom';
+import { Col, Container, Row } from 'react-bootstrap';
+import LeftNav from '../pages/Shared/LeftNav/LeftNav';
+import RightNav from '../pages/Shared/RightNav/RightNav';
 
 const RootLayout = () => {
     return (
         <div>
             <Header></Header>
+            <Container className='mt-3'>
+                <Row>
+                    <Col lg={3}>
+                        <LeftNav></LeftNav>
+                    </Col>
+                    <Col lg={6}>
+                        <Outlet></Outlet>
+                    </Col>
+                    <Col lg={3}>
+                        <RightNav></RightNav>
+                    </Col>
+                </Row>
+            </Container>
             <Footer></Footer>
         </div>
     );
