284133ab3fd763251b82046977cfd05231c423b0
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2023-11-01 12:56:08 +0600

    handle form submission and collect user registration data

--- src/userAuthentication/SignUp/SignUp.jsx
+++ src/userAuthentication/SignUp/SignUp.jsx
@@ -7,31 +7,50 @@ const SignUp = () => {
     //display dynamic title
     useSetTitle("Register");
 
+    //handle form submission & create a new user
+    const handleFormSubmission = (event) => {
+        event.preventDefault(); //prevent form default behaviour
+
+        console.log("clicked");
+
+        const userName = event.target.name.value;
+        const userEmail = event.target.email.value;
+        const userPassword = event.target.password.value;
+
+        // console.log(userName);
+
+    }
+
     return (
         <Container>
             <section>
                 <div>
-                    <form action=""></form>
-                    {/* name Field */}
-                    <Form.Group className="mb-3" controlId="name">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control type="email" placeholder="Enter Your Name here" className='w-25' />
-                    </Form.Group>
-                    {/* Email Field */}
-                    <Form.Group className="mb-3" controlId="email">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Enter Your Email here" className='w-25' />
-                    </Form.Group>
-                    {/* Password Field */}
-                    <Form.Group className="mb-3" controlId="password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Enter Your Password here" className='w-25' />
-                    </Form.Group>
-                    {/* Submit Button Field */}
-                    <Form.Group className="mb-3" controlId="submitBtn">
-                        <Button variant='dark' size='lg' active className='w-25'>Submit</Button>
-                    </Form.Group>
+                    <Form onSubmit={handleFormSubmission}>
 
+                        {/* name Field */}
+                        <Form.Group className="mb-3" controlId="name">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="name" placeholder="Enter Your Name here" className='w-25' />
+                        </Form.Group>
+
+                        {/* Email Field */}
+                        <Form.Group className="mb-3" controlId="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Enter Your Email here" className='w-25' />
+                        </Form.Group>
+
+                        {/* Password Field */}
+                        <Form.Group className="mb-3" controlId="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Enter Your Password here" className='w-25' />
+                        </Form.Group>
+
+                        {/* Submit Button Field */}
+                        <Form.Group className="mb-3" controlId="submitBtn">
+                            <Button variant='dark' type='submit' size='lg' className='w-25'>Submit</Button>
+                        </Form.Group>
+
+                    </Form>
                 </div>
             </section>
         </Container>
