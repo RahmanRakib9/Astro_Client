bba6467cf8b945cba4166c40a9c6adcf5e5e0169
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2023-11-01 12:40:16 +0600

    setup firebase config & utilize vite env

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+//firebase project configuration
+const firebaseConfig = {
+    apikey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
+}
+
+//initialize firebase
+export const app = initializeApp(firebaseConfig);
